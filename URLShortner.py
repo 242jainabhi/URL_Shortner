b76f6ec820ac9759573fee7a5a195c56092c006f
Abhishek Jain <abj@vectors.com>
2020-01-07 01:02:11 +0530

    MD5 hash with base62 encoding

--- URLShortner.py
+++ URLShortner.py
@@ -1,23 +1,59 @@
 import hashlib
-# import string
+import string
 
 class ShortenURL():
+    _PREFIX = 'http://tinyurl.com'
+    _ALPHABET = string.ascii_letters + string.digits
+    _BASE = len(_ALPHABET)
+
     def __init__(self):
-        self.dict = {}
+        self.tiny_to_long = {}
 
-    def long_to_short(self, long_url):
-        short_url = hashlib.md5(long_url.encode()).hexdigest()[-7:]
-        if short_url not in self.dict:
-            self.dict[short_url] = long_url
-        return short_url
+    def _convert_to_base(self, x):
+        result = []
+        while x:
+            x, r = divmod(x, ShortenURL._BASE)
+            result.append(ShortenURL._ALPHABET[r])
+        return ''.join(result)
 
-    def short_to_long(self, short_url):
-        if short_url in self.dict:
-            return self.dict[short_url]
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+
+        :type longUrl: str
+        :rtype: str
+        """
+        md5_hash = hashlib.md5(longUrl.encode()).hexdigest()
+        # md5_hash has 32 bytes
+        # get 3 slices of 10 bytes each and xor them so we end up with a shorter URL suffix of len 8
+        size = len(md5_hash)
+
+        x = md5_hash[:size//3]
+        y = md5_hash[size//3:-size//3]
+        z = md5_hash[-size//3:]
+        x_as_int = int(x, 16)
+        y_as_int = int(y, 16)
+        z_as_int = int(z, 16)
+        xored = x_as_int ^ y_as_int ^ z_as_int
+
+        suffix = self._convert_to_base(xored)
+        tiny_url = ShortenURL._PREFIX + '/' + suffix
+        if tiny_url not in self.tiny_to_long:
+            self.tiny_to_long[tiny_url] = longUrl
+
+        return tiny_url
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+
+        :type shortUrl: str
+        :rtype: str
+        """
+        return self.tiny_to_long.get(shortUrl)
 
 tiny_url = ShortenURL()
 str = 'Abhishe Jain'
 print('Original URL: ', str)
-shortened_url = tiny_url.long_to_short(str)
+
+shortened_url = tiny_url.encode(str)
 print('Shortened url: ', shortened_url)
-print('Original url: ', tiny_url.short_to_long(shortened_url))
+print('Original url: ', tiny_url.decode(shortened_url))
